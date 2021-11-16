0b7f9c86ffe94fa499807b7728feb4f2e8efad68
Sobha Atar <atarsobhan@gmail.com>
2021-11-16 13:37:47 +0330

    [WIP]add test - different entities make different keys

--- tests/MySQLStorageTest.php
+++ tests/MySQLStorageTest.php
@@ -34,7 +34,7 @@ class MySQLStorageTest extends TestCase
     /**
      * @test
      */
-    public function assert_multiple_request_create_single_hash_in_database(): void
+    public function assert_multiple_request_create_single_hash(): void
     {
         $i = 0;
         $config = [
@@ -87,7 +87,7 @@ class MySQLStorageTest extends TestCase
      * @test
      * @throws Exception
      */
-    public function assert_multiple_request_create_new_hash_in_database_after_ttl(): void
+    public function assert_multiple_request_create_new_hash_after_ttl(): void
     {
         $config = [
             'timeout' => 10,
@@ -122,5 +122,55 @@ class MySQLStorageTest extends TestCase
             ],
             'mysql');
         $this->assertDatabaseCount($config['table'], 2, 'mysql');
+    }
+
+
+    /**
+     * @test
+     */
+    public function assert_multiple_request_create_multiple_hash_with_different_entity(): void
+    {
+        $i = 0;
+        $config = [
+            'processes' => 5,
+            'timeout' => 10,
+            'ttl' => 100,
+            'host' => env('DB_HOST'),
+            'port' => env('DB_PORT'),
+            'db' => env('DB_DATABASE'),
+            'table' => config('idempotent.table'),
+            'user' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+        ];
+
+        for ($i = 0; $i < $config['processes']; $i++) {
+            $config['entities'][$i] = sprintf('request_%d', $i);
+        }
+
+        $this->loadMigrationsFrom(self::MIGRATION_PATH);
+
+        $pool = Pool::create();
+        for ($i = 0; $i < $config['processes']; $i++) {
+            $pool->add(function () use ($i, $config) {
+                $pdo = new \PDO(
+                    sprintf('mysql:host=%s;port=%d;dbname=%s', $config['host'], $config['port'], $config['db']),
+                    $config['user'],
+                    $config['password']
+                );
+                $service = new MysqlStorage($pdo, $config['table']);
+                return (new Idempotent())->verify($service, $config['entities'][$i], $config, 'some hash');
+
+            })->then(function ($output) {
+                if (!$output[0]) {
+                    $this->counter++;
+                }
+            })->catch(function (Exception $e) {
+                dump($e);
+            });
+        }
+        $pool->wait();
+        $this->assertEquals($config['processes'], $this->counter);
+        $this->assertEquals($config['processes'], $i);
+        $this->assertDatabaseCount($config['table'], $config['processes'], 'mysql');
     }
 }
