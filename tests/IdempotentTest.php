cf4085d1382bd15906dc65ab1bab111831aa2769
Sobha Atar <atarsobhan@gmail.com>
2021-11-16 21:04:11 +0330

    [WIP]add tests for Idempotent

---
+++ tests/IdempotentTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Sobhanatar\Idempotent\Tests;
+
+use Sobhanatar\Idempotent\{Contracts\MysqlStorage, Contracts\RedisStorage, Idempotent};
+
+class IdempotentTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function assert_storage_with_no_valid_connection_return_error(): void
+    {
+        $connection = 'invalid';
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage(sprintf('connection `%s` is not supported', $connection));
+
+        (new Idempotent())->resolveStorageService($connection);
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @test
+     */
+    public function assert_storage_with_redis_connection(): void
+    {
+        $connection = 'redis';
+        $service = (new Idempotent())->resolveStorageService($connection);
+
+        $this->assertInstanceOf(RedisStorage::class, $service);
+    }
+
+    /**
+     * @test
+     */
+    public function assert_storage_with_mysql_connection(): void
+    {
+        $connection = 'mysql';
+        $service = (new Idempotent())->resolveStorageService($connection);
+
+        $this->assertInstanceOf(MysqlStorage::class, $service);
+    }
+}
