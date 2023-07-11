8768cc46fd64f61186ef22bec351054904266323
haderianous <fandroid.j@gmail.com>
2023-07-11 17:30:23 +0330

    add mapper for response VerifyIdempotent

--- src/Middleware/VerifyIdempotent.php
+++ src/Middleware/VerifyIdempotent.php
@@ -44,17 +44,18 @@ class VerifyIdempotent
                 ->verify($this->config->getEntity(), $this->config->getEntityConfig(), $this->signature->getHash());
             if ($this->storageService->exists()) {
                 $response = $this->prepareResponse($this->config->getEntity(), $this->storageService->getResponse()['response']);
-                return fdb_success_api_response(__('idempotent.progress'), [$response], 200, 1, 1);
+                $statusCode = $this->storageService->getResponse()['code'] != null ? (int)$this->storageService->getResponse()['code'] : 401;
+                return $this->generateResponse(json_decode($response, true), $statusCode);
 //                return $this->response($request, $response, (int)$this->storageService->getResponse()['code']);
             }
 
             $response = $next($request);
             $storage->update($response, $this->config->getEntity(), $this->signature->getHash());
-            return fdb_success_api_response(__('idempotent.success'), ['message' => $response->getContent()], $response->getStatusCode(), 1, 1);
+            return $this->generateResponse(json_decode($response->getContent(), true), $response->getStatusCode());
 //            return $this->response($request, $response->getContent(), $response->getStatusCode());
 
         } catch (Exception $e) {
-            return fdb_fail_api_response(__('idempotent.fail'),__('idempotent.fail'));
+            return fdb_fail_api_response(__('idempotent.fail'), __('idempotent.fail'));
 //            return response(['message' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
@@ -69,6 +70,12 @@ class VerifyIdempotent
     public function prepareResponse(string $entity, ?string $response): string
     {
         return unserialize($response) ?? trans('idempotent.' . $entity);
+    }
+
+
+    public function generateResponse(array $response, int $statusCode): JsonResponse
+    {
+        return fdb_success_api_response($response['message'], $response['data']['result'], $statusCode, $response['data']['total'], $response['data']['per_page']);
     }
 
     /**
